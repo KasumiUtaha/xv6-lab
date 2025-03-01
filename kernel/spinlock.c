21c6cbdf1852a2e29c362753d4c7c6542efa9ac7
root <root@Utaha.>
2025-03-02 00:00:59 +0800

    Finish Locks lab

--- kernel/spinlock.c
+++ kernel/spinlock.c
@@ -63,7 +63,8 @@ acquire(struct spinlock *lk)
 {
   push_off(); // disable interrupts to avoid deadlock.
   if(holding(lk))
-    panic("acquire");
+    panic("acquire same lock");
+    
 
 #ifdef LAB_LOCK
     __sync_fetch_and_add(&(lk->n), 1);
